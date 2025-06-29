63f322d3b809d0e14937624adc9a2517e2d89f1c
suljevicsemir <34798880+suljevicsemir@users.noreply.github.com>
2025-06-29 17:36:26 +0200

    Feature/UI setup (#2)
    * added ui theme setup
    
    * added navigation setup
    
    * added ui components
    
    ---------
    
    Co-authored-by: Semir Suljevic <suljevic.semir@gmailcom>

---
+++ UI/src/main/java/com/semirsuljevic/ui/api/theme/AppSizes.kt
@@ -0,0 +1,18 @@
+package com.semirsuljevic.ui.api.theme
+
+import androidx.compose.ui.unit.dp
+
+object AppSizes {
+    val xxSmall = 2.dp
+    val xSmall = 4.dp
+    val small = 8.dp
+    val medium = 12.dp
+    val large = 16.dp
+    val xLarge = 24.dp
+    val xxLarge = 32.dp
+    val xxxLarge = 64.dp
+
+    val scaffoldHorizontal = 20.dp
+
+    val textFieldBorderWidth = 0.25.dp
+}

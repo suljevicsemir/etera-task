984a9705fc35360d0c02bb239e036a50dd4787b0
Semir Suljevic <suljevic.semir@gmailcom>
2025-07-01 04:35:34 +0200

    added new background and outline gradient

--- UI/src/main/java/com/semirsuljevic/ui/api/textField/AppTextField.kt
+++ UI/src/main/java/com/semirsuljevic/ui/api/textField/AppTextField.kt
@@ -6,12 +6,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.sp
 import com.semirsuljevic.ui.api.theme.AppSizes
 
 @Composable
@@ -19,8 +22,13 @@ fun AppTextField(
     onValueChange: (TextFieldValue) -> Unit,
     modifier: Modifier = Modifier,
     config: TextFieldConfig = TextFieldConfig(),
-    enabled: Boolean = true
-    
+    enabled: Boolean = true,
+    colors: TextFieldColors = OutlinedTextFieldDefaults.colors(
+        focusedContainerColor = Color.Transparent,
+        unfocusedContainerColor = Color.Transparent,
+        focusedBorderColor = MaterialTheme.colorScheme.onSurface,
+        unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
+    )
 ) {
     Column (
         horizontalAlignment = Alignment.Start
@@ -28,17 +36,24 @@ fun AppTextField(
         TextField(
             value = config.value,
             onValueChange = onValueChange,
-            textStyle = TextStyle(color = MaterialTheme.colorScheme.onBackground),
-            label = { Text(config.hint.asString(), color = MaterialTheme.colorScheme.onSurface) },
+            textStyle = TextStyle(
+                color = MaterialTheme.colorScheme.onPrimaryContainer,  
+                fontSize = 15.sp,
+                lineHeight = 15.sp,
+                fontWeight = FontWeight.W500,
+                letterSpacing = 0.2.sp
+            ),
+            label = { 
+                Text(
+                    config.hint.asString(),
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    fontSize = 11.sp,
+                )
+            },
             keyboardOptions = config.keyboardOptions, 
             visualTransformation = config.visualTransformation,
             enabled = enabled,
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = Color.Transparent,
-                unfocusedContainerColor = Color.Transparent,
-                focusedBorderColor = MaterialTheme.colorScheme.onSurface,
-                unfocusedBorderColor = MaterialTheme.colorScheme.onSurface
-            ),
+            colors = colors,
             modifier = modifier,
             
         )
